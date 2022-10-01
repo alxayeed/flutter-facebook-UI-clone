4566ebaa13cfa6c9924441f8e48b8d53a77bc411
alxayeed <alxayeed@gmail.com>
2022-10-01 19:30:32 +0600

    Appbar created

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,48 @@
+import 'package:facebook_clone/config/palette.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+import 'widgets/circle_button.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: true,
+            title: const Text(
+              "facebook",
+              style: TextStyle(
+                  color: Palette.facebookBlue,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: -1.2),
+            ),
+            systemOverlayStyle: const SystemUiOverlayStyle(
+                statusBarBrightness: Brightness.light),
+            backgroundColor: Colors.white,
+            actions: [
+              CircleButton(icon: Icons.search, onPressed: () {}),
+              CircleButton(icon: MdiIcons.facebookMessenger, onPressed: () {}),
+            ],
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => const ListTile(
+                title: Center(
+                  child: Text(''), //Text
+                ), //Center
+              ), //ListTile
+              childCount: 15,
+            ), //SliverChildBuildDelegate
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/widgets/circle_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CircleButton extends StatelessWidget {
+  final Function onPressed;
+  final IconData icon;
+  final double iconSize;
+  const CircleButton(
+      {Key? key,
+      required this.icon,
+      required this.onPressed,
+      this.iconSize = 30.0})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(6.0),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        shape: BoxShape.circle,
+      ),
+      // TODO: Align in center
+      child: IconButton(
+        onPressed: onPressed as void Function()?,
+        icon: Icon(
+          icon,
+          size: iconSize,
+          color: Colors.black,
+        ),
+      ),
+    );
+  }
+}
