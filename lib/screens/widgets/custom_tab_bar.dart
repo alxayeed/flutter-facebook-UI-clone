c32f265721b06afaf7ecf5ceeee2c7ff475b1d9b
alxayeed <alxayeed@gmail.com>
2022-10-05 11:31:47 +0600

    tabbar added

--- lib/screens/widgets/custom_tab_bar.dart
+++ lib/screens/widgets/custom_tab_bar.dart
@@ -14,20 +14,46 @@ class CustomTabBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: selectedTab,
-      selectedItemColor: Palette.facebookBlue,
-      unselectedItemColor: Colors.black45,
+    return TabBar(
+      // currentIndex: selectedTab,
+      indicatorColor: Palette.facebookBlue,
+      indicatorPadding: EdgeInsets.zero,
+      indicator: const BoxDecoration(
+        border: Border(
+          top: BorderSide(
+            color: Palette.facebookBlue,
+            width: 3.0,
+          ),
+        ),
+      ),
+      // unselectedItemColor: Colors.black45,
       onTap: onTap,
-      items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.ondemand_video), label: "Home"),
-        BottomNavigationBarItem(
-            icon: Icon(MdiIcons.accountCancelOutline), label: "Home"),
-        BottomNavigationBarItem(
-            icon: Icon(MdiIcons.bellOutline), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.menu), label: "Home"),
+      tabs: [
+        Icon(
+          Icons.home,
+          size: 30.0,
+          color: selectedTab == 0 ? Palette.facebookBlue : Colors.black45,
+        ),
+        Icon(
+          Icons.ondemand_video,
+          size: 30.0,
+          color: selectedTab == 1 ? Palette.facebookBlue : Colors.black45,
+        ),
+        Icon(
+          MdiIcons.accountCancelOutline,
+          size: 30.0,
+          color: selectedTab == 3 ? Palette.facebookBlue : Colors.black45,
+        ),
+        Icon(
+          MdiIcons.bellOutline,
+          size: 30.0,
+          color: selectedTab == 4 ? Palette.facebookBlue : Colors.black45,
+        ),
+        Icon(
+          Icons.menu,
+          size: 30.0,
+          color: selectedTab == 5 ? Palette.facebookBlue : Colors.black45,
+        ),
       ],
     );
   }
